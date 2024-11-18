84de8d3a06a25ba8fbe7c27ea47b081795beb8a5
kamoasko <kamdesigning@gmail.com>
2024-11-18 23:47:24 +0400

    navbar menu projects dropdown menu does not appears is fixed, trainings dropdown menu last items does not seem because of not scrollable fixed, target blank added to customers card link

---
+++ assets/index-Cspq_Ok9.js
@@ -0,0 +1,1 @@
+import{R as o,j as s,N as r}from"./index-BVNOneaU.js";const c="_tab_1fo9o_1",i={tab:c},x=o.memo(({title:t,to:a,onClick:e,isActive:n})=>s.jsx("li",{children:s.jsxs(r,{to:a,className:`${i.tab} tab flexCenter ${n?"active":""}`,onClick:e,children:[s.jsx("span",{children:t}),s.jsx("div",{})]})}));export{x as default};
